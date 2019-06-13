7f6682cfe8ca28cdb3846415a8996726a55e3c8d
pavel6767 <pavelmachuca@hotmail.com>
2019-06-13 17:22:06 -0400

    * fixed inputs alignment * fixed only one error message display

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+let errMessage = false;
 function validate() {
 	let userEmail = document.getElementById("user-email");
 	let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 	if (!userEmail.value.match(emailRegex)) {
-		let text = "Please provide a valid email address";
-		let newNode = document.createElement("p");
-		newNode.classList.add("text-red");
-		newNode.textContent = text;
-
-		userEmail.parentNode.insertBefore(newNode, userEmail.nextSibling);
+		if (!errMessage) {
+			errMessage = true;
+			let text = "Please provide a valid email address";
+			let newNode = document.createElement("p");
+			newNode.classList.add("text-red");
+			newNode.textContent = text;
+			userEmail.parentNode.insertBefore(newNode, userEmail.nextSibling);
+		}
 	} else {
 		console.log(userEmail.value);
 		console.log("passed");
 	}
+	return false;
 }
