b3bde1989be3e272848e43007326ecb0c4a94232
pavel6767 <pavelmachuca@hotmail.com>
2019-06-13 20:03:25 -0400

    added success/fail message

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-let errMessage = false;
+let err = false;
+let userEmail = document.getElementById("user-email");
+
+/* error message */
+let errText = "Please provide a valid email address";
+let suText = "Congratulations! You'll receive updates";
+let newNode = document.createElement("p");
+userEmail.parentNode.insertBefore(newNode, userEmail.nextSibling);
+
 function validate() {
-	let userEmail = document.getElementById("user-email");
 	let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 	if (!userEmail.value.match(emailRegex)) {
-		if (!errMessage) {
-			errMessage = true;
-			let text = "Please provide a valid email address";
-			let newNode = document.createElement("p");
+		if (!err) {
+			err = true;
 			newNode.classList.add("text-red");
-			newNode.textContent = text;
-			userEmail.parentNode.insertBefore(newNode, userEmail.nextSibling);
+			newNode.textContent = errText;
+			userEmail.style.border = "1px solid red";
 		}
 	} else {
+		err = false;
+		newNode.classList.remove("text-red");
+		newNode.textContent = suText;
+		userEmail.style.border = "1px solid black";
+
 		console.log(userEmail.value);
 		console.log("passed");
 	}
+
 	return false;
 }
